5957183dd2a19027d4c01f51eb57a87cdcca7045
Anton Smirnov <sandfox+git@sandfox.me>
2025-09-26 22:17:25 +0300

    Test every discovery branch

--- tests/HistoricalRequestTest.php
+++ tests/HistoricalRequestTest.php
@@ -69,4 +69,60 @@ final class HistoricalRequestTest extends TestCase
             $response->exception->getMessage(),
         );
     }
+
+
+    public function testDateDiscovery(): void
+    {
+        $cache = new Psr16Cache(new ArrayAdapter());
+        $http = MockClient::get();
+        $clock = StaticClock::fromDateString('2025-09-26'); // 'now'
+
+        $service = new NationalBankOfRomaniaService(cache: $cache, httpClient: $http, clock: $clock);
+
+        // last 10 records test
+
+        // exact date
+        $response = $service->send(new HistoricalExchangeRateRequest('EUR', 'RON', Calendar::parse('2025-09-24')));
+        self::assertInstanceOf(ExchangeRateResponse::class, $response);
+        self::assertEquals('5.0784', $response->rate->value);
+        self::assertEquals('2025-09-24', $response->date->toString());
+        self::assertCount(1, $http->getRequests());
+
+        // discovery
+        $response = $service->send(new HistoricalExchangeRateRequest('EUR', 'RON', Calendar::parse('2025-09-21')));
+        self::assertInstanceOf(ExchangeRateResponse::class, $response);
+        self::assertEquals('5.0719', $response->rate->value);
+        self::assertEquals('2025-09-19', $response->date->toString());
+        self::assertCount(1, $http->getRequests());
+
+        // miss
+        $response = $service->send(new HistoricalExchangeRateRequest('EUR', 'RON', Calendar::parse('2025-09-14')));
+        self::assertInstanceOf(ExchangeRateResponse::class, $response);
+        self::assertEquals('5.0694', $response->rate->value);
+        self::assertEquals('2025-09-12', $response->date->toString());
+        self::assertCount(2, $http->getRequests()); // loaded the year 2025
+
+        // year data
+
+        // exact date
+        $response = $service->send(new HistoricalExchangeRateRequest('EUR', 'RON', Calendar::parse('2012-06-13')));
+        self::assertInstanceOf(ExchangeRateResponse::class, $response);
+        self::assertEquals('4.4593', $response->rate->value);
+        self::assertEquals('2012-06-13', $response->date->toString());
+        self::assertCount(3, $http->getRequests()); // loaded the year 2012
+
+        // discovery
+        $response = $service->send(new HistoricalExchangeRateRequest('EUR', 'RON', Calendar::parse('2012-08-19')));
+        self::assertInstanceOf(ExchangeRateResponse::class, $response);
+        self::assertEquals('4.4852', $response->rate->value);
+        self::assertEquals('2012-08-17', $response->date->toString());
+        self::assertCount(3, $http->getRequests());
+
+        // miss
+        $response = $service->send(new HistoricalExchangeRateRequest('EUR', 'RON', Calendar::parse('2012-01-02')));
+        self::assertInstanceOf(ExchangeRateResponse::class, $response);
+        self::assertEquals('4.3197', $response->rate->value);
+        self::assertEquals('2011-12-30', $response->date->toString());
+        self::assertCount(4, $http->getRequests()); // loaded the year 2011
+    }
 }
